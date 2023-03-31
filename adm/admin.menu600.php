9a8a17ea3676c764437bd91071f8df983ca059e3
SeongKon <canoa950327@gmail.com>
2023-03-31 16:30:30 +0900

    관리자페이지 커스텀화 진행중

--- adm/admin.menu600.php
+++ adm/admin.menu600.php
@@ -8,7 +8,7 @@ $menu['menu600'] = array (
     array('600300', '마이닝지급 및 내역', ''.G5_ADMIN_URL.'/bonus/bonus_mining.php','bbs_board'),
     array('600150', '패키지 상품관리', G5_ADMIN_URL.'/shop_admin/itemlist.php', 'scf_item'),
     array('600400', '패키지 상품 현황', ''.G5_ADMIN_URL.'/bonus/rank_table.php','bbs_board'),
-    array('600800', '센터관리',''.G5_ADMIN_URL.'/bonus/bonus.center_member.php'),
+    // array('600800', '센터관리',''.G5_ADMIN_URL.'/bonus/bonus.center_member.php'),
     array('600500', '승급 현황', ''.G5_ADMIN_URL.'/bonus/member_upgrade.php','bbs_board'),
     // array('600300', '프로모션 제한리셋 기록', G5_ADMIN_URL.'/m3cron_log.php')
     
@@ -27,7 +27,6 @@ $menu['menu600'] = array (
         array('600400', '패키지 상품 현황', ''.G5_ADMIN_URL.'/bonus/rank_table.php','bbs_board'),
         array('600800', '센터관리',''.G5_ADMIN_URL.'/bonus/bonus.center_member.php'),
         array('600500', '승급 현황', ''.G5_ADMIN_URL.'/bonus/member_upgrade.php','bbs_board'),
-        
     );
 }
 ?>
