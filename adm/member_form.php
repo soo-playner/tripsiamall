57d01a82151c7289d67b6ff7b66f177bbc7d4f02
SeongKon <canoa950327@gmail.com>
2023-04-04 15:50:56 +0900

    Admin, Client 개인정보&보안설정 업데이트

--- adm/member_form.php
+++ adm/member_form.php
@@ -3,7 +3,7 @@ $sub_menu = "200100";
 include_once('./_common.php');
 include_once(G5_THEME_PATH . '/_include/wallet.php');
 include_once(G5_PATH . '/util/package.php');
-include_once(G5_PLUGIN_PATH.'/Encrypt/rule.php');
+include_once(G5_PLUGIN_PATH . '/Encrypt/rule.php');
 
 
 auth_check($auth[$sub_menu], 'w');
@@ -16,8 +16,9 @@ function bonus_pick($val)
 	return $list;
 }
 
-function checkerble($val,$bool){
-	if($val == $bool){
+function checkerble($val, $bool)
+{
+	if ($val == $bool) {
 		return "checked";
 	}
 }
@@ -366,8 +367,8 @@ $rank_result = sql_fetch($rank_sql);
 						<? if ($w == "u") { ?>
 							<input type="hidden" name="mb_id" id="mb_id" value="<?= $mb['mb_id'] ?>" />
 							<span class='td_id <? if ($mb['mb_divide_date'] != '') {
-													echo 'red';
-												} ?>'><?= $mb['mb_id'] ?></span>
+																		echo 'red';
+																	} ?>'><?= $mb['mb_id'] ?></span>
 
 						<? } else { ?>
 							<input type="text" name="mb_id" value="<?php echo $mb['mb_id'] ?>" id="mb_id" <?php echo $required_mb_id ?> class="frm_input <?php echo $required_mb_id_class ?>" size="15" minlength="3" maxlength="20">
@@ -378,9 +379,7 @@ $rank_result = sql_fetch($rank_sql);
 
 					<th scope="row"><label for="mb_id">이름<?php echo $sound_only ?></label></th>
 					<td>
-						<? if ($w == "u") { ?>
-							<input type="text" name="mb_name" id="mb_name" class='frm_input wide' value="<?= $mb['mb_name'] ?>" />
-						<? } ?>
+						<span class="td_id"><?= $mb['mb_name'] ?></span>
 					</td>
 
 				</tr>
@@ -403,7 +402,8 @@ $rank_result = sql_fetch($rank_sql);
 		<? } ?>
 		-->
 					</td>
-				
+					<th scope="row"><label for="mb_email">휴대폰 번호<strong class="sound_only">필수</strong></label></th>
+					<td><input type="text" name="mb_hp" value="<?php echo $mb['mb_hp'] ?>" id="mb_hp" maxlength="13" class="frm_input wide" size="13"></td>
 				</tr>
 
 				<tr>
@@ -450,8 +450,8 @@ $rank_result = sql_fetch($rank_sql);
 		<th scope="row">센터지정</th>
 		<td colspan="1">
 			<input type="checkbox" style='width:24px;height:24px' name="center_use" id="center_use" value=" <?= $mb['center_use'] ?> " class="frm_input" <? if ($mb['center_use'] == '1') {
-																																							echo "checked";
-																																						} ?> />
+																																																																											echo "checked";
+																																																																										} ?> />
 			<? if ($mb['center_use'] > 0) {
 				$center_regist_class = 'active';
 			} else {
@@ -552,7 +552,7 @@ $rank_result = sql_fetch($rank_sql);
 			width: auto;
 		}
 	</style>
-  
+
 	<tr class="ly_up padding-box fund">
 		<th scope="row">보유 잔고</th>
 
@@ -569,11 +569,11 @@ $rank_result = sql_fetch($rank_sql);
 		<td></td>
 	</tr>
 
-  <tr class="ly_up padding-box fund">
+	<tr class="ly_up padding-box fund">
 		<th scope="row">잔고 수동지급/차감</th>
 
 		<td colspan="1">
-      <input type="hidden" name="mb_deposit_point_math" id="math_code" value="">
+			<input type="hidden" name="mb_deposit_point_math" id="math_code" value="">
 			<input type="button" value="+" class='math_btn plus'>
 			<input type="button" value="-" class='math_btn minus'>
 			<input type="text" name="mb_deposit_point_add" value="" id="field_upstair" class="frm_input wide" size="15" style="max-width:60%" inputmode=price>
@@ -944,8 +944,8 @@ $rank_result = sql_fetch($rank_sql);
 		<!-- 	<tr>
 		<th scope="row">회원구분표시</th>
 		<td><input type="checkbox" style='width:24px;height:24px' name="center_use" id="center_use" value=" <?= $mb['center_use'] ?> " class="frm_input" <? if ($mb['center_use'] == '1') {
-																																							echo "checked";
-																																						} ?> /></td>
+																																																																												echo "checked";
+																																																																											} ?> /></td>
 		<th scope="row">회원출금차단</th>
 		<td></td>
 	</tr> -->
@@ -1003,33 +1003,32 @@ this.form.mb_intercept_date.value=this.form.mb_intercept_date.defaultValue; }">
 	<tr>
 		<th scope="row"><label for="mb_memo">KYC인증</label></th>
 		<td colspan="1">
-			<?if($mb['kyc_cert'] > 0){
+			<? if ($mb['kyc_cert'] > 0) {
 				echo kyc_cert($mb['kyc_cert']);
-				echo person_key($mb['mb_id'],$mb['kyc_cert'],$mb['person_key']);
-				echo "<span style='color:gray;margin-left:20px;'>".$mb['kyc_regdt']."</span>";
-			}else{
-				echo person_key($mb['mb_id'],$mb['kyc_cert'],$mb['person_key']);	
-			}?>
+				echo person_key($mb['mb_id'], $mb['kyc_cert'], $mb['person_key']);
+				echo "<span style='color:gray;margin-left:20px;'>" . $mb['kyc_regdt'] . "</span>";
+			} else {
+				echo person_key($mb['mb_id'], $mb['kyc_cert'], $mb['person_key']);
+			} ?>
 
-			<?if($mb['person_key'] != '0'){
+			<? if ($mb['person_key'] != '0') {
 				$kyc_cert = sql_fetch("SELECT * FROM g5_write_kyc WHERE mb_id = '{$mb['mb_id']}' ORDER BY wr_id desc limit 0,1 ");
-				
-				if($kyc_cert){
-					echo "<a href =/adm/bbs/board.php?bo_table=kyc&wr_id=".$kyc_cert['wr_id']." class='line_btn' style='padding:3px 5px;margin-left:5px;'>바로가기</a>";
-				}	
 
-			}?>
+				if ($kyc_cert) {
+					echo "<a href =/adm/bbs/board.php?bo_table=kyc&wr_id=" . $kyc_cert['wr_id'] . " class='line_btn' style='padding:3px 5px;margin-left:5px;'>바로가기</a>";
+				}
+			} ?>
 		</td>
 
 		<th scope="row"><label for="mb_memo">KYC수동처리</label></th>
 		<td colspan="1">
-			<input type="checkbox" id="kyc_admin_1" name="kyc_admin" class="kyc_admin_btn" <?=checkerble(1,$mb['kyc_cert']);?> onclick='checkOnlyOne(this)'value="1">
+			<input type="checkbox" id="kyc_admin_1" name="kyc_admin" class="kyc_admin_btn" <?= checkerble(1, $mb['kyc_cert']); ?> onclick='checkOnlyOne(this)' value="1">
 			<label for="kyc_admin_1">인증회원</label>
 
-			<input type="checkbox" id="kyc_admin_2" name="kyc_admin" class="kyc_admin_btn" <?=checkerble(2,$mb['kyc_cert']);?> onclick='checkOnlyOne(this)' value="2">
+			<input type="checkbox" id="kyc_admin_2" name="kyc_admin" class="kyc_admin_btn" <?= checkerble(2, $mb['kyc_cert']); ?> onclick='checkOnlyOne(this)' value="2">
 			<label for="kyc_admin_2">미승인</label>
 
-			<input type="checkbox" id="kyc_admin_0" name="kyc_admin" class="kyc_admin_btn" <?=checkerble(0,$mb['kyc_cert']);?> onclick='checkOnlyOne(this)'value="0">
+			<input type="checkbox" id="kyc_admin_0" name="kyc_admin" class="kyc_admin_btn" <?= checkerble(0, $mb['kyc_cert']); ?> onclick='checkOnlyOne(this)' value="0">
 			<label for="kyc_admin_0">등록대기</label>
 		</td>
 	</tr>
@@ -1058,17 +1057,16 @@ this.form.mb_intercept_date.value=this.form.mb_intercept_date.defaultValue; }">
 
 <script>
 	function checkOnlyOne(element) {
-  
-		const checkboxes 
-			= document.getElementsByName("kyc_admin");
-		
+
+		const checkboxes = document.getElementsByName("kyc_admin");
+
 		checkboxes.forEach((cb) => {
 			cb.checked = false;
 		})
-		
+
 		element.checked = true;
 	}
-	$('input:checkbox[name=kyc_admin]').on('change', function(){
+	$('input:checkbox[name=kyc_admin]').on('change', function() {
 		alert('관리자 수동 변경시 기존 KYC 회원인증보다 우선처리됩니다.');
 	});
 
@@ -1141,15 +1139,15 @@ this.form.mb_intercept_date.value=this.form.mb_intercept_date.defaultValue; }">
 				alert("최초입금처리시에는 바로처리되지 않으며,\n입금 요청 내역에서 승인처리하여야 정상입금처리됩니다. ");
 			}
 		}
-		
-		
-		$('input:checkbox[name=kyc_admin]').each(function (index) {
-			if($(this).is(":checked")==true){
+
+
+		$('input:checkbox[name=kyc_admin]').each(function(index) {
+			if ($(this).is(":checked") == true) {
 				console.log($(this).val());
 				f.kyc_admin.value = $(this).val();
 			}
 		});
-		
+
 
 		// return true;
 	}
