6bafcf2842a21ff47256c4d9fd435da9a9d0c00a
SeongKon <canoa950327@gmail.com>
2023-04-10 14:01:12 +0900

    패키지 구매 시 해당 패키지로 받을 수 있는 수당 한도 300% 설정

--- util/upstairs_proc.php
+++ util/upstairs_proc.php
@@ -95,9 +95,12 @@ if($rst && $logic){
 	}else{
 		$update_rank = $val;
 	}
+
+	// 해당 패키지로 받을 수 있는 수당 한도(300%)
+	$max_save_point = $it_point * 3;
 	
 	$update_point .= ", mb_rate = ( mb_rate + {$pv}) ";
-	$update_point .= ", mb_save_point = ( mb_save_point + {$it_point}) ";
+	$update_point .= ", mb_save_point = ( mb_save_point + {$max_save_point}) ";
 	$update_point .= ", rank = '{$update_rank}', rank_note = '{$pack_name}', sales_day = '{$now_datetime}' ";
 	$update_point .= " where mb_id ='".$mb_id."'";
 
