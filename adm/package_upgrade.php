291dc96478d5bb71c1648bf676b48d346f1b5334
jinho <tazzaenciel@gmail.com>
2023-05-12 17:05:22 +0900

    [수정] 관리자페이지 상품 업그레이드시 에러처리

--- adm/package_upgrade.php
+++ adm/package_upgrade.php
@@ -8,6 +8,7 @@ $it_id = $_POST['it_id'] ? sql_escape($_POST['it_id']) : false;
 
 if(!$mb_id || !$it_id){
  echo json_encode(array("code"=>0001));
+ return false;
 }
 
 $sql = "select od_id, count(*) as cnt from g5_shop_order where mb_id = '{$mb_id}' and od_tno = {$it_id} order by od_date asc limit 0,1";
@@ -15,6 +16,7 @@ $row = sql_fetch($sql);
 
 if($row['cnt'] <= 0){
     echo json_encode(array("code"=>0001));
+    return false;
 }
 
 $_POST['od_id'] = $row['od_id'];
